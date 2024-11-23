58dcb5993d78e7ea7a921683603cc4d8c3e51e10
eamonmooney <eamonmooney.ea@gmail.com>
2024-11-23 16:34:12 +0000

    Refactored shopCollisions

--- droneDeliveryExtreme/Form1.cs
+++ droneDeliveryExtreme/Form1.cs
@@ -233,21 +233,13 @@ namespace droneDeliveryExtreme
         }
 
         //Detecting if the plyer is currently hovering over a purchaseable upgrade
-        bool shopCollisions(int left, int right, int top, int bottom)
-        {
-            PictureBox[] shopPic = { shopBattery, shopDrone, shopBullets };
-            int shopNum = shopPic.Length;
-            for (int count = 0; count < shopNum; count++)
-            {
-                if ((left < shopPic[count].Right) &&
-                (right > shopPic[count].Left) &&
-                (top < shopPic[count].Bottom) &&
-                (bottom > shopPic[count].Top))
-                {
-                    return true;
-                }
-            }
-            return false;
+        bool shopCollisions(int left, int right, int top, int bottom){
+            PictureBox[] shopPics = { shopBattery, shopDrone, shopBullets };
+            for (PictureBox shopPic in shopPics){
+                if ((left < shopPic.Right) &&
+                (right > shopPic.Left) &&
+                (top < shopPic.Bottom) &&
+                (bottom > shopPic.Top)){return true;}}return false;
         }
 
         //Identifies what upgrade the player is hovering over
